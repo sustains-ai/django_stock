2ec99065ecc6d03db63a875132772cc48225601a
Arjun Chandran Unni <arjunchandranunni@Arjuns-MacBook-Pro.local>
2025-01-29 15:24:53 +0400

    Changed portfolio path

--- sandbox_2/sandbox_2/settings.py
+++ sandbox_2/sandbox_2/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "portfolio",
+    "sandbox_2.portfolio",
 ]
 
 MIDDLEWARE = [
