90c581294ab964e91deeeeacab11efd7b0ee53ed
sustains <arjuncrevathi@gmail.com>
2025-05-05 10:55:04 +0400

    Changed from SQLITE to postgres

--- sandbox_2/config/settings.py
+++ sandbox_2/config/settings.py
@@ -83,12 +83,17 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600,
+        conn_health_checks=True
+    )
 }
+
 
 
 # Password validation
